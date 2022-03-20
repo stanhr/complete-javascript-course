d9ffdd0eba9861d5f29dcacc3c03f6561ab78ed9
Stas <gs@se7ensky.com>
2022-03-21 01:11:43 +0200

    day1

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,32 @@
+/*
+// alert("hello world");
+
+console.log("hello world!");
+
+let data = 1;
+if (data === 1) (console.log(true))
+else (console.log(false));
+
+// Assignments
+const country = "Ukraine";
+const continent = "Europe";
+let population = 40;
+console.log(country, continent, population);
+const isIsland = false;
+const language = "ukrainian";
+console.log(typeof(country), typeof(continent), typeof(population), typeof(isIsland), typeof(language));
+console.log(language);
+
+// LECTURE: Basic Operators
+let splitPopulation = population / 2;
+splitPopulation++;
+console.log(splitPopulation);
+let finlandPopulation = 6;
+let averagePopulation = 33;
+console.log(population < finlandPopulation);
+console.log(averagePopulation > population);
+
+// LECTURE: Strings and Template Literals
+let description = `${country} is in ${continent}, and its ${population} million people speak ${language}.`;
+console.log(description);
+*/
