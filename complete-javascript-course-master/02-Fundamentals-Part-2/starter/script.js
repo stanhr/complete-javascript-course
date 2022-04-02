e4da29ba0d36a6881b4db9321bb6bb71728d6b6d
noname <>
2022-04-02 17:50:14 +0300

    // Coding Challenge #3 — Fundamentals Part 2

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -188,7 +188,7 @@ const myCountry = {
 
 console.log(`${myCountry.name} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry["capital"]}.`);
 
-*/
+
 
 // Object Methods Lection Challenge
 
@@ -213,3 +213,34 @@ const jonas = {
 };
 
 console.log(jonas.summary());
+
+*/
+
+// Coding Challenge #3 — Fundamentals Part 2
+
+const mark = {
+    fullName: "Mark Miller",
+    mass: 78,
+    height: 1.69,
+
+    calcBMI: function () {
+        this.bmi = this.mass / (this.height * this.height);
+        return this.bmi;
+    }
+}
+
+const john = {
+    fullName: "John Smith",
+    mass: 92,
+    height: 1.95,
+
+    calcBMI: function () {
+        this.bmi = this.mass / (this.height * this.height);
+        return this.bmi;
+    }
+}
+
+mark.calcBMI();
+john.calcBMI();
+
+console.log(`John's BMI (${john.bmi}) is ${john.bmi > mark.bmi ? "higher" : "lower"} than Mark's (${mark.bmi})`);
