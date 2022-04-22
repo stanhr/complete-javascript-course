f9a17ab24f6a7c5fa54109027d276a60f2471adf
Stas <gs@se7ensky.com>
2022-04-22 23:25:33 +0300

    11

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+// secret number
+document.querySelector(".number").textContent; 
+
+// message (start guessing)
+document.querySelector(".message").textContent;
+
+// score field
+document.querySelector(".score").textContent;
+
+// high score field
+document.querySelector(".highscore").textContent;
+
+// receive data from input (value)
+document.querySelector(".guess").value;
+
+
+// Генерация дефолтного рендомного числа от 0 до 20 включительно.
+const number = Math.trunc(Math.random()*20+1)
+document.querySelector(".number").textContent = number;
+
+// кнопка "check", adding event listener for click, and calling function after click — эта функция вернет значение (как обычно функция возвращает).
+document.querySelector(".check").addEventListener("click", function(){
+    document.querySelector(".guess").value;
+    // value = возьмет значение введенное в инпут с классом .guess
+})
